5eb04571445d41e0bbf11cf748e347a3a6758328
Guillaume R <guillaume.rondon@gmail.com>
2020-03-21 21:09:03 +0100

    ♻️ refacto avec react hooks

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import React, { Component } from 'react'
-import axios from 'axios'
-import './App.css'
+import React from 'react'
+import { useState } from 'react';
+import axios from 'axios';
+import './App.css';
 
-class App extends Component {
-  constructor () {
-    super()
-    this.state = {
-      wind_speed: '',
-      wind_deg: ''
 
-    }
-    this.handleClick = this.handleClick.bind(this)
-  }
-  
-  handleClick () {
-    axios.get('http://api.openweathermap.org/data/2.5/forecast?zip=13100,fr&APPID=f4e38397f380af9bab3b4dc73fab548a')
-      .then(response => this.setState({
-        wind_speed: response.data.list[0].wind.speed,
-        wind_deg: response.data.list[0].wind.deg
-      }))
-  }
-  
-  render() {
-    return (
-      <div className='button__container'>
-        <button id='btn_forecast' className='button' onClick={this.handleClick}>Forecast</button>
-        <p id='speed_today'>{this.state.wind_speed}</p>
-        <p id='deg_today'>{this.state.wind_deg}</p>
+
+function App() {
+  const [windDirection, setDirection] = useState(0);
+  const [windSpeed, setSpeed] = useState(1);
+
+  return (
+    <div>
+      <button id='btn_forecast' className='button' onClick={() =>
+        axios.get('http://api.openweathermap.org/data/2.5/forecast?zip=13100,fr&APPID=f4e38397f380af9bab3b4dc73fab548a')
+          .then(response => {
+            setSpeed(response.data.list[0].wind.speed)
+            setDirection(response.data.list[0].wind.deg)
+          })}>Forecast</button>
+      <div>
+          <p id='speed_today'>{windSpeed}</p>
+          <p id='deg_today'>{windDirection}</p>
       </div>
-    )
-  }
+    </div>
+  );
+
 }
 
 export default App
