933be91c6738e2dddb07ec5f91e50de8677cf824
Guillaume R <guillaume.rondon@gmail.com>
2020-03-21 22:12:25 +0100

    🎨 ajout de bootstrap

--- src/App.js
+++ src/App.js
@@ -10,16 +10,19 @@ function App() {
   const [windSpeed, setSpeed] = useState(1);
 
   return (
-    <div>
-      <button id='btn_forecast' className='button' onClick={() =>
-        axios.get('http://api.openweathermap.org/data/2.5/forecast?zip=13100,fr&APPID=f4e38397f380af9bab3b4dc73fab548a')
-          .then(response => {
-            setSpeed(response.data.list[0].wind.speed)
-            setDirection(response.data.list[0].wind.deg)
-          })}>Forecast</button>
-      <div>
-          <p id='speed_today'>{windSpeed}</p>
-          <p id='deg_today'>{windDirection}</p>
+    <div class="jumbotron jumbotron-fluid">
+      <div class="container">      <div className="card">
+        <div className="card-body">
+          <p class="card-text" id='speed_today'>{windSpeed}</p>
+          <p class="card-text" id='deg_today'>{windDirection}</p>
+        </div>
+        <button id='btn_forecast' class="btn btn-primary" onClick={() =>
+          axios.get('http://api.openweathermap.org/data/2.5/forecast?zip=13100,fr&APPID=f4e38397f380af9bab3b4dc73fab548a')
+            .then(response => {
+              setSpeed(response.data.list[0].wind.speed)
+              setDirection(response.data.list[0].wind.deg)
+            })}>Forecast</button>
+      </div>
       </div>
     </div>
   );
