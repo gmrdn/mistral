37aa2c8b5d304e80d4acd80d638ce7045618d67e
Guillaume R <guillaume.rondon@gmail.com>
2020-03-22 12:44:01 +0100

    ajout test mistral / pas mistral & montées de version CI

--- cypress/integration/forecast_spec.js
+++ cypress/integration/forecast_spec.js
@@ -1,20 +1,49 @@
 /// <reference types="Cypress" />
 
-context('Forecast', () => {
+describe('Forecast pas de mistral', () => {
   beforeEach(() => {
     cy.server();
-    cy.route('**/forecast*','fixture:bouchon_aix.json')
+    cy.route('**/forecast*','fixture:bouchon_aix_pas_mistral.json')
     cy.visit('/')
     cy.get('#btn_forecast').click();
   })
 
   it('should display today\'s wind speed', () => {
+    cy.get('#is_mistral')
+      .should('contain', 'Y\'a pas de mistral')
     cy.get('#speed_today')
-      .should('contain', '2.01');
+      .should('contain', '7 km/h');
+
   })
 
   it('should display today\'s wind direction', () => {
+    cy.get('#is_strong')
+      .should('contain', 'Ca souffle pas fort')
     cy.get('#deg_today')
       .should('contain', '137.603');
+  })
+})
+
+describe('Forecast avec mistral', () => {
+  beforeEach(() => {
+    cy.server();
+    cy.route('**/forecast*','fixture:bouchon_aix_mistral.json')
+    cy.visit('/')
+    cy.get('#btn_forecast').click();
+  })
+
+  it('should display today\'s wind speed', () => {
+    cy.get('#is_mistral')
+      .should('contain', 'Y\'a du mistral')
+    cy.get('#speed_today')
+      .should('contain', '21 km/h');
+
+  })
+
+  it('should display today\'s wind direction', () => {
+    cy.get('#is_strong')
+      .should('contain', 'Ca souffle fort')
+    cy.get('#deg_today')
+      .should('contain', '290.603');
   })
 })
